7af098dbaadb467ba4209deea9fde37959188a8b
FarhanNisar59 <farhan.nisar24@gmail.com>
2025-03-26 16:16:55 +0500

    first commit

--- resources/views/website/car-listing/car-listing-filters/car-list.blade.php
+++ resources/views/website/car-listing/car-listing-filters/car-list.blade.php
@@ -1,14 +1,15 @@
 @foreach($cars as $car)
 <div class="col-sm-6 col-md-6 col-lg-4 mb-5 car-item">
     <div class="car-listing-card">
+        <a href="{{ url('/cardetail/' . $car->id) }}" class="link">
         <img src="{{ asset('storage/' . $car->thumbnail) }}" alt="{{ $car->car_models->name ?? 'Car Image' }}" class="listing-car-image mb-2">
-        
+        </a>
         <div class="car-info">
             <div class="d-flex justify-content-between bg-light align-items-center rounded">
                 <h6 class="car-price">${{ $car->rent }}/day</h6>
-                <button class="book-btn" onclick="window.location.href='{{ url('/cardetail/' . $car->id) }}'">Book</button>
+                <button class="book-btn" onclick="window.location.href='#'">Book</button>
             </div>
-            
+            <a href="{{ url('/cardetail/' . $car->id) }}" class="link">
             <h5 class="car-name">{{ $car->car_models->name ?? 'Unknown Model' }}</h5>
             
             <div class="car-details">
@@ -29,7 +30,7 @@
                     {{ ucfirst($car->transmission) }}
                 </div>
             </div>
-    
+        </a>
             <button onclick="window.location.href='{{ url('/cardetail/' . $car->id) }}'" class="details-btn rounded-pill">
                 Details
             </button>            
