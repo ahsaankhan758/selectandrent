8f2036934e2e526d4950a17b037e33de1246af67
Ihsan younas <ahsaankhan758@gmail.com>
2025-04-16 12:33:49 +0500

    header changes and booking

--- public/frontend-assets/assets/Js/getDifferenceDays.js
+++ public/frontend-assets/assets/Js/getDifferenceDays.js
@@ -1,18 +1,20 @@
-const pickupInput = document.getElementById('getPickupDate');
+document.addEventListener('DOMContentLoaded', function () {
+    const pickupInput = document.getElementById('getPickupDate');
     const dropoffInput = document.getElementById('getDropoffDate');
     const result = document.getElementById('timeDifference');
 
     function calculateDifference() {
+        
         const pickupDate = new Date(pickupInput.value);
         const dropoffDate = new Date(dropoffInput.value);
 
         if (pickupInput.value && dropoffInput.value && dropoffDate > pickupDate) {
-            const diffMs = dropoffDate - pickupDate; // in milliseconds
-            const diffHrs = diffMs / (1000 * 60 * 60); // hours
-            const diffDays = Math.floor(diffHrs / 24); // days
+            const diffMs = dropoffDate - pickupDate;
+            const diffHrs = diffMs / (1000 * 60 * 60);
+            const diffDays = Math.floor(diffHrs / 24);
             const remainingHours = Math.floor(diffHrs % 24);
 
-            result.innerText = `Difference: ${diffDays} day(s) and ${remainingHours} hour(s)`;
+            result.innerText = `Duration: ${diffDays} day(s) and ${remainingHours} hour(s)`;
         } else if (pickupInput.value && dropoffInput.value) {
             result.innerText = `Drop-off must be after Pickup.`;
         } else {
@@ -20,5 +22,8 @@ const pickupInput = document.getElementById('getPickupDate');
         }
     }
 
-    pickupInput.addEventListener('change', calculateDifference);
-    dropoffInput.addEventListener('change', calculateDifference);
+    if (pickupInput && dropoffInput) {
+        pickupInput.addEventListener('change', calculateDifference);
+        dropoffInput.addEventListener('change', calculateDifference);
+    }
+});
