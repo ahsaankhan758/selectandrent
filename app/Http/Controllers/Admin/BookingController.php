714c40db4306a2af3fb9ae319bde90c6de074242
FarhanNisar59 <farhan.nisar24@gmail.com>
2025-05-15 18:54:23 +0500

    graph by default and loctaion name and company show its booking

--- app/Http/Controllers/Admin/BookingController.php
+++ app/Http/Controllers/Admin/BookingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 use App\Models\Booking;
+use App\Models\BookingItem;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Auth;
@@ -9,25 +10,49 @@ class BookingController extends Controller
 {
     public function index()
     {
+        $query = Booking::with(['booking_items.vehicle.carModel', 'user', 'car']);
 
-        $query = Booking::orderBy('created_at', 'desc');
-    
         if (Auth::user()->role === 'company') {
-            $query->where('user_id', Auth::id());
+            $query->whereHas('booking_items.vehicle', function ($q) {
+                $q->where('user_id', Auth::id());
+            });
         }
-    
-        $bookings = $query->with('user')->paginate(10);
-    
-        return(view('admin.carBooking.carBooking', compact('bookings')));
-        
-        // $bookings = Booking::with('user')->paginate(10);
-        // return(view('admin.carBooking.carBooking', compact('bookings')));
+
+        $bookings = $query->paginate(10);
+
+        return view('admin.carBooking.carBooking', compact('bookings'));
     }
 
-    public function carBookingDetail($id)
-    {
-        $booking = Booking::with(['booking_items.vehicle.carModel', 'user','car'])->findOrFail($id);
-        return view('admin.carBooking.carBookingDetail', compact('booking'));
-    }
+    // public function carBookingDetail($id)
+    // {
+    //     $booking = Booking::with(['booking_items.vehicle.carModel', 'user','car'])->findOrFail($id);
+    //     return view('admin.carBooking.carBookingDetail', compact('booking'));
+    // }
+public function carBookingDetail($id)
+{
+    $booking = Booking::with([
+        'booking_items.vehicle.carModel', 
+        'booking_items.pickupLocation',
+        'booking_items.dropoffLocation',
+        'user', 
+        'car'
+    ])->findOrFail($id);
+
+    return view('admin.carBooking.carBookingDetail', compact('booking'));
+}
+
+
+//     public function carBookingDetail($id)
+// {
+//     $booking = Booking::with(['booking_items.vehicle.carModel', 'user', 'car'])->findOrFail($id);
+
+//     if (auth()->user()->role === 'company') {
+//         $booking->booking_items = $booking->booking_items->filter(function ($item) {
+//             return $item->vehicle && $item->vehicle->user_id == auth()->id();
+//         })->values(); 
+//     }
+
+//     return view('admin.carBooking.carBookingDetail', compact('booking'));
+// }
 
 }
