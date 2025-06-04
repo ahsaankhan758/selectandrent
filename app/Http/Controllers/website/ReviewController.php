585fa4c6c9bc0af9871e8ffc5e25a8f715670b59
Ihsan younas <ahsaankhan758@gmail.com>
2025-06-04 11:16:24 +0500

    new updates

--- app/Http/Controllers/website/ReviewController.php
+++ app/Http/Controllers/website/ReviewController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\website;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Review;
+use App\Models\Car;
 
 class ReviewController extends Controller
 {
@@ -36,6 +37,20 @@ class ReviewController extends Controller
             'rating' => $validated['rating'],
             'comment' => $validated['comment'],
         ]);
+
+        // Step 3: Send notification to vehicle company 
+        if ($validated['vehicle_id']) {
+
+            $car = Car::with('users')->find($validated['vehicle_id']);
+            $notificationType = 5; // review save against vehicle car
+            $fromUserId = auth()->id(); // logged in user
+            $toUserId = $car->user_id;
+            $userId = $car->user_id; 
+            $message = 'A new review from ('.auth()->user()->name.') has been submitted for your Vehicle: ' . $car->lisence_plate;
+            saveNotification($notificationType, $fromUserId, $toUserId, $userId, $message);
+
+        }
+
 
         return response()->json([
             'status' => 'success',
