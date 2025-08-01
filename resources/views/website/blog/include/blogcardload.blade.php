684475fb62bae4124ecce317a14d6e0ba384b684
ahmed javed <britishbhutta@gmail.com>
2025-08-01 11:26:09 +0500

    Blog_Updated

--- resources/views/website/blog/include/blogcardload.blade.php
+++ resources/views/website/blog/include/blogcardload.blade.php
@@ -2,7 +2,16 @@
 <div class="col-lg-4 col-md-6 mb-4">
     <div class="blog-card">
         <a href="{{ route('blog.detail', $blog->id) }}">
-            <img src="{{ asset('storage/' . $blog->thumbnail) }}" class="blog-image" alt="Blog Image">
+            @php
+                $path = public_path('storage/' . $blog->thumbnail);
+                $imageExists = $blog->thumbnail && file_exists($path);
+            @endphp
+
+            @if ($imageExists)
+                <img src="{{ asset('storage/' . $blog->thumbnail) }}" class="blog-image" alt="Blog Image">
+            @else
+                <img src="{{ asset('images/no-image-blog.avif') }}" class="custom-card-img" alt="No Image Available">
+            @endif
         </a>                    
         <div class="date-box">
             {{ date('d', strtotime($blog->date)) }} <br> {{ date('F', strtotime($blog->date)) }}
