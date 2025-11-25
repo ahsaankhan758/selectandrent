976e6b736fa8cb32fcb7b66a55254b362adf931d
britishbhutta <britishbhutta@gmail.com>
2025-11-25 18:54:41 +0500

    Changes

--- public/assets/js/admin/locationArea.js
+++ public/assets/js/admin/locationArea.js
@@ -1,30 +1,50 @@
-$(document).ready(function() {
-    $('select[name="location"]').prop('disabled', true); 
+var oldCity = window.oldCity;
+var oldLocation = window.oldLocation;
 
+$(document).ready(function() {
+
+    if(!oldCity){
+        $('select[name="location"]').prop('disabled', true);
+    }
+
+    if (oldCity) {
+        $('select[name="city"]').val(oldCity);
+        loadArea(oldCity, true);
+    }
+
+    // Standard city change event
     $('select[name="city"]').change(function() {
-        var city_id = $(this).val(); 
-        loadArea(city_id);
-    
+        var city_id = $(this).val();
+        loadArea(city_id, false); 
     });
-    function loadArea(city_id){
+
+    function loadArea(city_id, restoringOld){
         if (city_id) {
             $.ajax({
-                url: window.getLocationsUrl + '/' + city_id, 
+                url: window.getLocationsUrl + '/' + city_id,
                 type: 'GET',
                 dataType: 'json',
                 success: function(data) {
-                 
-                    $('select[name="location"]').prop('disabled', false);
-                    
-                    $('select[name="location"]').empty();
 
-                    $('select[name="location"]').append('<option value="" selected>Select Area</option>');
+                    $('select[name="location"]').prop('disabled', false);
+                    $('select[name="location"]').empty();
+                    $('select[name="location"]').append('<option value="">Select Area</option>');
 
                     $.each(data, function(key, value) {
-                        $('select[name="location"]').append('<option value="' + value.id + '">' + value.area_name + '</option>');
+
+                        let selected = '';
+
+                        // Only select old value the FIRST time (restoring)
+                        if (restoringOld && oldLocation && value.id == oldLocation) {
+                            selected = 'selected';
+                        }
+
+                        $('select[name="location"]').append(
+                            '<option value="' + value.id + '" ' + selected + '>' + value.area_name + '</option>'
+                        );
                     });
                 },
-                error: function(xhr, status, error) {
+                error: function(xhr) {
                     console.log(xhr.responseText);
                 }
             });
@@ -34,4 +54,7 @@ $(document).ready(function() {
             $('select[name="location"]').append('<option value="" selected>Select Area</option>');
         }
     }
+});
+$(document).on('submit', 'form', function () {
+    $('#location, #city').prop('disabled', false);
 });
